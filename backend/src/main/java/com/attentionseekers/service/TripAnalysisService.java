0ddc255024ece5b659b6042871f8b552b49bb74e
Matei Aruxandei <aruxandei@student.tudelft.nl>
2025-10-05 01:54:54 +0200

    Working functionality

--- backend/src/main/java/com/attentionseekers/service/TripAnalysisService.java
+++ backend/src/main/java/com/attentionseekers/service/TripAnalysisService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class TripAnalysisService {
